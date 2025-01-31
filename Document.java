7f21dd466d2b127f5af2bb465abba2d778325e85
MatteoHiro <matteoo.izzo@gmail.com>
2025-01-31 16:12:34 +0100

    Codice da pulire, db funzionante

--- Document.java
+++ Document.java
@@ -1,4 +1,4 @@
-
+// Commit
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-
+// Commit
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
